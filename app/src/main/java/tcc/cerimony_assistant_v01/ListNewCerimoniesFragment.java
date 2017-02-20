30da28a4dbc1706363356ec482c98d5f1ccc009c
jr- <joseluis.ruas@gmail.com>
2017-02-20 00:04:14 -0300

    IT01 - Listar Novas Cerimonias v0.1, listagem concluida, sem clicklistener, tarefa ainda nao terminada

---
+++ app/src/main/java/tcc/cerimony_assistant_v01/ListNewCerimoniesFragment.java
@@ -0,0 +1,58 @@
+package tcc.cerimony_assistant_v01;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ListNewCerimoniesFragment extends Fragment {
+
+    private ArrayAdapter<String> mNewCerimoniesAdapter;
+
+    public ListNewCerimoniesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] fileNames = new String[0];
+        try {
+            fileNames = getActivity().getAssets().list("new");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        List<String> newCerimoniesList = new ArrayList<String>(Arrays.asList(fileNames));
+
+        // Now that we have some dummy forecast data, create an ArrayAdapter.
+        // The ArrayAdapter will take data from a source (like our dummy forecast) and
+        // use it to populate the ListView it's attached to.
+        mNewCerimoniesAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_newcerimonies, // The name of the layout ID.
+                        R.id.list_item_newcerimonies_textview, // The ID of the textview to populate.
+                        newCerimoniesList);
+
+        View rootView = inflater.inflate(R.layout.fragment_list_new_cerimonies, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_newcerimonies);
+        listView.setAdapter(mNewCerimoniesAdapter);
+
+        return rootView;
+    }
+}
