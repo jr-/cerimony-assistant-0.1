9ca5a51ae1ed89bacf5e761416bc88a5e3c97c4f
jr- <joseluis.ruas@gmail.com>
2018-02-18 16:47:57 -0300

    i1t1 displays full table layout with content

--- app/src/main/java/tcc/cerimony_assistant_v01/ManageParticipantsFragment.java
+++ app/src/main/java/tcc/cerimony_assistant_v01/ManageParticipantsFragment.java
@@ -7,6 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageParticipantsFragment extends Fragment {
 
@@ -20,6 +28,21 @@ public class ManageParticipantsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_manage_participants, container, false);
+        String jString = ParticipantsJSONParser.convertJSONFiletoString();
+        List<Participant> participants = ParticipantsJSONParser.getParticipantsFromJSON(jString);
+
+
+        TableLayout table = (TableLayout)view.findViewById(R.id.tableLayout);
+
+        for(int i = 0; i < participants.size(); i++) {
+            TableRow row = (TableRow)LayoutInflater.from(getActivity()).inflate(R.layout.attrib_row_manage_participants, null);
+            ((TextView)row.findViewById(R.id.text_name)).setText(participants.get(i).getPName());
+            ((TextView)row.findViewById(R.id.text_email)).setText(participants.get(i).getEmail());
+            ((TextView)row.findViewById(R.id.text_unit)).setText(participants.get(i).getUnidade());
+
+            table.addView(row, i+1);
+        }
+        table.requestLayout();
         return view;
     }
 }
--- app/src/main/java/tcc/cerimony_assistant_v01/ParticipantsJSONParser.java
+++ app/src/main/java/tcc/cerimony_assistant_v01/ParticipantsJSONParser.java
@@ -1,11 +1,19 @@
 package tcc.cerimony_assistant_v01;
 
+import android.os.Environment;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 /**
@@ -17,6 +25,36 @@ public class ParticipantsJSONParser {
     private static final String TAG_SNAME = "sname";
     private static final String TAG_SEMAIL = "semail";
     private static final String TAG_SORGANIZATION = "sorganization";
+    public static String convertJSONFiletoString() {
+        //convert participants.json in a string
+        File file = new File(Environment.getExternalStorageDirectory(), "ceremony-assistant/participants.json");
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String jString = "";
+        try {
+            FileChannel fc = stream.getChannel();
+            MappedByteBuffer bb = null;
+            try {
+                bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            /* Instead of using default, pass in a decoder. */
+            jString = Charset.defaultCharset().decode(bb).toString();
+        }
+        finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return jString;
+    }
     public static ArrayList<Participant> getParticipantsFromJSON(String json) {
         if (json != null) {
             try {
