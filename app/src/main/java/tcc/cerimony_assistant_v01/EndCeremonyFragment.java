cf5088d5a72e9c8641e720603a661cdc05ad7e62
jr- <joseluis.ruas@gmail.com>
2018-03-27 13:14:48 -0300

    I2T8, add button to return to list ceremonies activity, text to display status

--- app/src/main/java/tcc/cerimony_assistant_v01/EndCeremonyFragment.java
+++ app/src/main/java/tcc/cerimony_assistant_v01/EndCeremonyFragment.java
@@ -1,10 +1,13 @@
 package tcc.cerimony_assistant_v01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -20,7 +23,22 @@ public class EndCeremonyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_end_ceremony, container, false);
+        Cerimony ceremony = CCerimonies.getInstance().getSelectedCerimony();
 
+        //dinamically modify GUI
+        getActivity().setTitle("Fechamento da Ata - " + ceremony.getShortName());
+        TextView status_tv = (TextView) rootView.findViewById(R.id.status);
+        //if
+        status_tv.setText("A Cerimônia terminou com sucesso!");
+
+        Button btn = (Button) rootView.findViewById(R.id.return_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ListNewCerimonies.class);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
 }
